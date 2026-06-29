c327936b03525dc388dca6332e170ba4be921a07
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-06-29 13:39:06 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l = 0,r= people.size()-1;
+        int cnt = 0;
+        while(l<=r)
+        {
+            int w =(l!=r)? people[l]+people[r] : people[l];
+            if(w<=limit)
+            {
+                l++;
+                r--;
+                cnt++;
+            }
+            else
+            {
+                r--;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
