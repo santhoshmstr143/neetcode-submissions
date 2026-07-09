ae2a27c42e6ea372d056e6fa126f1dd5a70dca1a
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-07-09 15:50:15 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int l =0, r = 0;
+        while(r < nums.size()-1)
+        {
+            int far = 0;
+            for(int i=l;i<=r;i++)
+            {
+                far = max(far,nums[i]+i);
+            }
+            l = r+1;
+            r = far;
+            jumps++;
+        }
+        return jumps;
+    }
+};
