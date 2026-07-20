a358a4a3db1f690d804b294bfbf0726cb6451cd9
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-07-20 15:33:55 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool dfs(int node,vector<vector<int>> & adj, vector<int> &vis,int p)
+    {
+        vis[node]=1;
+        for(auto it : adj[node])
+        {
+            if(!vis[it]) 
+            {
+                if(dfs(it,adj,vis,node)) return true;
+            }
+            else if(it!=p)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size()!=n-1) return false;
+        vector<vector<int>> adj(n);
+        for(auto it : edges)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> vis(n,0);
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                cnt++;
+                if(dfs(i,adj,vis,-1)) return false;
+            }
+        }
+        return cnt==1;        
+    }
+};
