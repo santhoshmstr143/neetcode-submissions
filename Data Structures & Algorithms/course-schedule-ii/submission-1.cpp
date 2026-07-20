e1e0afdf1dbbdcf5d478fec1dd8d72b78a9e8e72
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-07-20 14:55:04 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& a) {
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        for(auto it : a)
+        {
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(ans.size()==n)
+        {
+            return ans;
+        }
+
+        return {};
+    }
+};
