3190bc2aa573c3680af50385cc743f757f3f76aa
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-07-24 11:11:00 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(int i,int n,int k,vector<vector<int>> &ans,vector<int> &temp)
+    {
+        if(temp.size()==k) 
+        {
+            ans.push_back(temp);
+            return;
+        }
+        if(i==n+1) return;
+        
+        temp.push_back(i);
+        solve(i+1,n,k,ans,temp);
+        temp.pop_back();
+        
+        solve(i+1,n,k,ans,temp);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(1,n,k,ans,temp);
+        return ans;
+    }
+};
