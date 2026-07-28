d835bc91d2c77ba13edc0fc8a5b3e05c3a5af914
Mule Mohan Venkata Santosh Reddy <santhoshreddy.mstr@gmail.com>
2026-07-28 14:22:19 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int dfs(int n,vector<int>&dp)
+    {
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = dfs(n-1,dp) + dfs(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return dfs(n,dp);
+    }
+};
